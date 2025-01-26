ad359ab0310b903c62e7ee3c05caf5d46868251e
PedroSantosFilho <psfilho81@hotmail.com>
2025-01-26 10:13:46 -0300

    Create alo_mundo01.py

---
+++ exercicios_modulo05/alo_mundo01.py
@@ -0,0 +1,6 @@
+###Exercícios de estrutura sequêncial
+
+#### 1. Faça um Programa que mostre a mensagem (print) "Alo mundo" na tela.
+print("\nExercíos de estrutura sequêncial: ")
+# imprime Alo mundo na tela
+print("Alo mundo\n")
