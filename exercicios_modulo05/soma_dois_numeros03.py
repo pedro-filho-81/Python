7665040f8fa17a3474c3972101aa994be157aefa
PedroSantosFilho <psfilho81@hotmail.com>
2025-01-26 10:14:49 -0300

    Create soma_dois_numeros03.py

---
+++ exercicios_modulo05/soma_dois_numeros03.py
@@ -0,0 +1,7 @@
+#### 3. Faça um Programa que peça dois números e imprima a soma.
+print("\nExibe a soma de 2 números")
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+soma = (n1 + n2)
+# resultado
+print(f"A soma de {n1} + {n2} = {soma}\n")
