454a32d06b2101a5c8263067fa03677e0fd4b375
PedroSantosFilho <psfilho81@hotmail.com>
2025-01-26 10:14:38 -0300

    Create qual_numero_digitado02.py

---
+++ exercicios_modulo05/qual_numero_digitado02.py
@@ -0,0 +1,5 @@
+#### 2. Faça um Programa que peça um número (input) e então mostre a mensagem: "O número informado foi [número]."
+# entrada
+numero = input("\nDigite um número: ")
+# resultado
+print(f"O número informado foi {numero}")
