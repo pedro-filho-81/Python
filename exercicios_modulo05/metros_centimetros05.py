088bcd6ffeaff4c0dca7f74760ef8e1faed3003b
PedroSantosFilho <psfilho81@hotmail.com>
2025-01-26 10:14:32 -0300

    Create metros_centimetros05.py

---
+++ exercicios_modulo05/metros_centimetros05.py
@@ -0,0 +1,7 @@
+#### 5. Faça um Programa que converta metros para centímetros. Você pode pedir o comprimento em metros para o usuário (input).
+print("\nmetros para centímetros.")
+metros = float(input("Informe o comprimento em metros: "))
+# conversão metros para centimetros * 0.100
+centimetros = metros * 100
+# exibe resultado
+print(f"{metros}m equivale a {centimetros}cm\n")
